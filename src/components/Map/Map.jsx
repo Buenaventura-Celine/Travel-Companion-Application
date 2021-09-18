3c0dee1e433e58910a3e2694e37f6a60462afaaf
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-18 17:45:07 +0800

    Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 0, lng: 0 }
+
     return (
-        <h1>
-            Map
-        </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBwB4rW10LhP8C3lInmjNIb08DGZKNEoJc' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50 , 50 ,  50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     )
 }
 
