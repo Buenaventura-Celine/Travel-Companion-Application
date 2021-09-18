c85ed99e58b508dfa1f1dcecca3848b53e733622
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-18 15:59:54 +0800

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Header = () => {
-    return (
-        <h1>
-            Header
-        </h1>
-    )
-}
+import useStyles from './styles.js';
 
-export default Header
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Wanderlust
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
