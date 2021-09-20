c6dfdf3cc82d86e03b003da620b350c657b9f5f5
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-20 08:47:59 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,4 @@ export const getPlacesData = async (type, sw, ne) => {
     console.log(error);
   }
 };
+
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,27 +9,25 @@ const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" className={classes.appbar}>
+    <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Wanderlust
+          Travel Advisor
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
   );
 };
-
-export default Header;
